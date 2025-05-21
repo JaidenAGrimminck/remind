07573abcfff78b45b902e55fd3cf0abe81d1c6d7
JaidenAGrimminck <jaidng1@gmail.com>
2025-05-22 00:42:41 +0200

    Initial Commit

---
+++ Remind/RemindApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindApp.swift
+//  Remind
+//
+//  Created by Jaiden Grimminck on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
